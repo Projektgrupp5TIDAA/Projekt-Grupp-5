c46c3f2258fb1c257a9c18b5531384137c8146a4
SL4GG4N <eddie.lowenborg@live.se>
2015-04-16 09:39:54 +0200

    TEST SDL
    provar ett fönster

--- BJORNSTHEGAME/bjornsclient.c
+++ BJORNSTHEGAME/bjornsclient.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
+#include <sdl2/sdl.h>
+#include <sdl2_net/sdl_net.h>
 
 int main(int argc, char const *argv[])
 {
-    printf("Hej grupp 5");
+	SDL_Window* window;
+	SDL_INIT_VIDEO;
+    window = SDL_CreateWindow("HEJ",						//Window name
+    						SDL_WINDOWPOS_UNDEFINED,		//x-position
+    						SDL_WINDOWPOS_UNDEFINED,		//y-position
+    						720,
+    						400,
+    						SDL_WINDOW_OPENGL				//flags etc
+    						);
+
+    if (window==NULL)
+    {
+    	printf("Couldnt create window\n");
+    }
+    SDL_Delay(8000); //8 sek
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 	return 0;
 }

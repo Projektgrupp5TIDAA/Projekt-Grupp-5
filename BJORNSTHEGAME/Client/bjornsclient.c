f7d2943d6be1d9a3e2c36b1a557d31bd8c2a4c4d
jonathankahre <jonte.k_1993@hotmail.com>
2015-04-17 18:04:45 +0200

    Fixed text

--- BJORNSTHEGAME/Client/bjornsclient.c
+++ BJORNSTHEGAME/Client/bjornsclient.c
@@ -1,51 +1,44 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
-#include <SDL2_ttf/SDL_ttf.h>
-
+#include <SDL2/SDL_image.h>
+#include "testheader.h"
 
 int main(int argc, char const *argv[])
 {
-    SDL_Window* window;
-    SDL_INIT_VIDEO;
-    window = SDL_CreateWindow("HEJ",						//Window name
-                              SDL_WINDOWPOS_UNDEFINED,		//x-position
-                              SDL_WINDOWPOS_UNDEFINED,		//y-position
-                              720,
-                              400,
-                              SDL_WINDOW_FULLSCREEN_DESKTOP	//flags etc
-                              );
-    
-    SDL_Renderer *test = SDL_CreateRenderer(window, -1, 0);
-    
-    if (window==NULL)
+  SDL_Window* window;
+  SDL_Init(SDL_INIT_EVERYTHING);
+  TTF_Init();
+  window = SDL_CreateWindow("HEJ",				//Window name
+			    SDL_WINDOWPOS_UNDEFINED,		//x-position
+			    SDL_WINDOWPOS_UNDEFINED,		//y-position
+			    600,
+			    300,
+			    0	//flags etc
+			    );
+  if (window==NULL)
     {
-        printf("Couldnt create window\n");
+      printf("Window could not be created.\n");
     }
-    
-    TTF_Init();
-    TTF_Font *font;
-    font = TTF_OpenFont("Metropolitan-Text.ttf", 16);
-    SDL_Color color={0,0,0};
-    SDL_Surface *text_surface;
-    SDL_Texture* testmessage = SDL_CreateTextureFromSurface(test, text_surface);
-    SDL_Rect testrect;
-    testrect.x = 0;
-    testrect.y = 0;
-    testrect.w = 100;
-    testrect.h = 100;
-    if(!(text_surface=TTF_RenderText_Solid(font, "Hello!", color))){
-        printf("FEEEL\n");
-    } else {
-        SDL_RenderCopy(test, testmessage, NULL, &testrect);
-    }
-    
-    
-    SDL_Delay(8000); //8 sek
-    
-    
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-    return 0;
-    
+  
+  SDL_Surface *screen = SDL_GetWindowSurface(window);
+  SDL_Color white={255,255,255};
+  TTF_Font *font = TTF_OpenFont("Metropolian-Text.ttf", 40);
+  if(font == NULL){
+    printf("Font can't be found or is in an unknown format.\n");
+    exit(0);
+  }
+  SDL_Surface *text = TTF_RenderText_Solid(font, "I FIXED IT LOL", white);
+  SDL_Surface *image = IMG_Load("yellow_player1.png");
+  SDL_Rect imagedestination = {0,50,0,0};
+  SDL_BlitSurface(image, NULL, screen, &imagedestination);
+  SDL_BlitSurface(text, NULL, screen, NULL);
+  SDL_UpdateWindowSurface(window);
+  SDL_Delay(8000); //8 sek
+  
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+  TTF_Quit();
+  return 0;
+  
 }

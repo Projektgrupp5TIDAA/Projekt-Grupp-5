9424a78e590943d39ec942a1408b0349e44f96b4
jonathankahre <jonte.k_1993@hotmail.com>
2015-04-17 13:10:46 +0200

    Server updates aka. chatclient ver

--- BJORNSTHEGAME/Server/bjornserver.c
+++ BJORNSTHEGAME/Server/bjornserver.c
@@ -7,16 +7,14 @@
 #include "bjornthreads.h"
 
 int main(int argc, char **argv){
-  UDPsocket sock[PLAYERCOUNT + 1];       /* Socket descriptor */
-  UDPpacket *mem;       /* Pointer to packet memory */
-  pinfo players[PLAYERCOUNT];
-  int ports[PLAYERCOUNT + 1];
-  getPorts(ports);
-  unsigned char clear='0';
-  int open[PLAYERCOUNT + 1] = {0};
-  int quit[PLAYERCOUNT + 1] = {0};
-  int check_thread, threadID=0, i;
-  pthread_t threads[PLAYERCOUNT + 1]; // array av trådar
+  UDPsocket socket;       // Socket
+  UDPpacket *packet;       // Pointer to the allocated space of the socket
+  pinfo players[PLAYERCOUNT]; // Array of player-structs
+  int ports[PLAYERCOUNT + 1]; // Array of ports
+  getPorts(ports); //Populate the port-array
+  char clear='0'; //char to clear the data input
+  int quit=0, check_thread, threadID=0, i; //various ints
+  pthread_t threads[PLAYERCOUNT + 1]; // Array av threads
   
   /* Initialize SDL_net */
   if (SDLNet_Init() < 0){
@@ -25,50 +23,47 @@ int main(int argc, char **argv){
   }
   
   /* Open the sockets */
-  for(i=0;i<PLAYERCOUNT+1;i++){
-    if (!(sock[i] = SDLNet_UDP_Open(ports[i]))){
-      fprintf(stderr, "SDLNet_UDP_Open: %s\n", SDLNet_GetError());
-      exit(EXIT_FAILURE);
-    }
-    open[0]=1;
+  if (!(socket = SDLNet_UDP_Open(ports[0]))){
+    fprintf(stderr, "SDLNet_UDP_Open: %s\n", SDLNet_GetError());
+    exit(EXIT_FAILURE);
   }
-  /* Make space for the packets on the sockets */
-  if (!(mem = SDLNet_AllocPacket(512))){
+  
+  /* Allocate space for the sockets */
+  if (!(packet = SDLNet_AllocPacket(512))){
     fprintf(stderr, "SDLNet_AllocPacket: %s\n", SDLNet_GetError());
     exit(EXIT_FAILURE);
   }
   
   /* Main loop */
-  while (!quit[0]){
-    /* Waits for a packet. UDP_Recv returns != 0 if a packet is incoming */
-    mem->data = &clear;
-    if (SDLNet_UDP_Recv(sock[0], mem)){
+  while (!quit){
+    /* Clears the data input then waits for a packet */
+    packet->data = &clear;
+    if (SDLNet_UDP_Recv(socket, packet)){
       /* Copies the data and compares it to the request-string*/
-      if(strstr("COMMREQ", mem->data) && threadID < 7){
-	/* If the request is valid, a thread is spawned and assigned a socket with a port-number */
+      if(strstr("COMMREQ", packet->data) && threadID < 6){
+	/* If the request is valid, a thread is spawned and assigned a port */
 	printf("Request recieved, assigning port.\n");
-	initiatePlayer(&(players[threadID]));
+	initiatePlayer(&(players[threadID])); //Initiate the player-struct
 	players[threadID].threadID = threadID;
-	players[threadID].sock = &sock[threadID+1];//+1 because 0 is main
-	open[threadID] = 1;
+	players[threadID].port = ports[threadID+1];//+1 because 0 is main
 	char sendport[4];
 	sprintf(sendport, "%d", ports[threadID+1]);
-	mem->data = sendport;
-	SDLNet_UDP_Send(sock[threadID+1], -1, mem);
+	packet->data = sendport;
+	SDLNet_UDP_Send(socket, -1, packet);
 	pthread_create(&threads[threadID],NULL,&check_ports,(void *)&players[threadID]);
 	threadID++;
-      }else printf("Invalid request recieved, discarding.\n");
+      }else printf("Invalid request or server is full, discarding.\n");
       
       /* Quit if packet contains the exit-command */
-      if (strcmp((char *)mem->data, "EXIT") == 0)
-	quit[0] = 1;
+      if (strcmp((char *)packet->data, "EXIT") == 0)
+	quit = 1;
     }		
   }
   int j;
   for(j=0;j<threadID;j++)
     pthread_join(threads[threadID], NULL);
   /* Clean and exit */
-  SDLNet_FreePacket(mem);
+  SDLNet_FreePacket(packet);
   SDLNet_Quit();
   return EXIT_SUCCESS;
 }

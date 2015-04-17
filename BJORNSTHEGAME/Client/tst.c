0f4a7e0d45e87685f8155bbe4bc3e720da8bb7a0
nooraa <noora-70@live.se>
2015-04-17 14:15:22 +0200

    .

--- BJORNSTHEGAME/Client/tst.c
+++ BJORNSTHEGAME/Client/tst.c
@@ -9,13 +9,14 @@
 #define SHOOTING 0
 #define WALKING 1
 #define JUMPING 2
-SDL_Rect position; 
+#define Timer 10
+SDL_Rect position;
 struct player
 {
-    int x, y, dx;
+    int x, y, dy;
     char name[WORDLENGTH];
     int plife;
-    int shooting, walking, jumping;
+    int shooting, walking, jumping, current, turningLeft;
     char alive[WORDLENGTH];
 
 };
@@ -33,8 +34,7 @@ void life(Player *p, bool shooted)// counting HP (players life)
     if(HP==0)
     {
         //gameOver?
-         strcpy(p-> alive, "No");
-        
+        strcpy(p-> alive, "No");
     }
     else
     {
@@ -44,9 +44,9 @@ void life(Player *p, bool shooted)// counting HP (players life)
 
 int Bullets(Player p)//how many bullets left to the player?
 {
-   
+
     int newBullets=0;
-    if(p.shooting ==0)
+    if(p.shooting ==1)
     {
         newBullets= BULLETS-1;
     }
@@ -56,11 +56,11 @@ int Bullets(Player p)//how many bullets left to the player?
 
 bool shootedP()
 {
-  
+
     bool n=false;
     Player p1, p2;
     position.x=p1.x;
-    if(p1.shooting==0 && p2.x==position.x)
+    if(p1.shooting==1 && p2.x==position.x)
     {
         n=true;
     }
@@ -73,15 +73,124 @@ bool shootedP()
     return n;
 }
 
+int Events(SDL_Window *window, Player *p)
+{
+    SDL_Event event;
+    int done = 0;
+
+    while(SDL_PollEvent(&event))
+    {
+        switch(event.type)
+        {
+        case SDL_WINDOWEVENT_CLOSE:
+        {
+            if(window)
+            {
+                SDL_DestroyWindow(window);
+                window = NULL;
+                done = 1;
+            }
+        }
+        break;
+        case SDL_KEYDOWN:
+        {
+            switch(event.key.keysym.sym)
+            {
+            case SDLK_ESCAPE:
+                done = 1;
+                break;
+            }
+        }
+        break;
+        case SDL_QUIT:
+            //out of the game
+            done = 1;
+            break;
+        }
+    }
+
+    const Uint8 *s = SDL_GetKeyboardState(NULL);
+    if(!p->shooting)
+    {
+        if(s[SDL_SCANCODE_LEFT])
+        {
+            p->x -= 3;
+            p->walking = 1;
+            p->turningLeft = 1;
+
+            if(Timer % 10 == 0)
+            {
+                p->current++;
+                p->current %= 5;
+            }
+        }
+        else if(s[SDL_SCANCODE_RIGHT])
+        {
+            p->x += 3;
+            p->walking = 1;
+            p->turningLeft = 0;
+
+            if(Timer % 10 == 0)
+            {
+                p->current++;
+                p->current %= 5;
+            }
+        }
+        else
+        {
+            p->walking = 0;
+            p->current = 5;
+        }
+    }
+
+    if(!p->walking)
+    {
+        if(s[SDL_SCANCODE_SPACE])
+        {
+            if(Timer %10== 0)
+            {
+                if(p->current == 5)
+                    p->current = 6;
+                else
+                    p->current = 5;
+
+                if(!p->turningLeft)
+                {
+
+                    p->shooting = 1;
+
+                }
+            }
+
+        }
+        else
+        {
+            p->current = 5;
+            p->shooting = 0;
+        }
+    }
+
+    if(s[SDL_SCANCODE_UP] && !p->dy)
+    {
+        p->dy = -8;
+    }
+    if(s[SDL_SCANCODE_DOWN])
+    {
+        p->y += 10;
+    }
+
+    return done;
+}
+
 int main(int argc, char const *argv[])
 {
 
-  bool shoots=false;
-  Player p1= {10,20,15,"Martin",5,SHOOTING, WALKING,JUMPING,"yes"};//test
-  shoots=shootedP();
-  life(&p1, shoots);
-  
-  
+    /* bool shoots=false;
+    Player p1= {10,20,15,"Martin",5,SHOOTING, WALKING,JUMPING,"yes"};//test
+    shoots=shootedP();
+    life(&p1, shoots);*/
+
+
 
     return 0;
 }

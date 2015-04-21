e80ce40bda1e305fee046aa8570f2c573caaa7c6
nooraa <noora-70@live.se>
2015-04-21 16:39:21 +0200

    hit

---
+++ BJORNSTHEGAME/Client/hit.h
@@ -0,0 +1,27 @@
+#ifndef HIT_H_INCLUDED
+#define HIT_H_INCLUDED
+
+bool shut(){
+  const Uint8 *state= SDL_GetKeyboardState(NULL);
+  bool shooting= true;
+ 
+  if(state[SDL_SCANCODE_X]==true ){
+    shooting = true;
+      
+  }
+  return shooting;
+}
+
+bool hit(){
+  bool x = false;
+  SDL_Rect position;
+  Player p1,p2;
+  position.x = p1.x;
+  if(shut()==true && position.x ==p2.x){
+    x = true;
+
+ }
+  return x;
+
+}
+

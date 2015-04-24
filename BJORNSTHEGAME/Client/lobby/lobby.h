562df4bee378a3aba483e16d90ca22aa2f60e9ef
SL4GG4N <eddie.lowenborg@live.se>
2015-04-24 14:44:03 +0200

    sync

--- BJORNSTHEGAME/Client/lobby/lobby.h
+++ BJORNSTHEGAME/Client/lobby/lobby.h
@@ -1,7 +1,17 @@
-#ifndef	_LOBBY_
+#ifndef _LOBBY_
 #define _LOBBY_
 #include <stdio.h>
 #include <SDL2/SDL.h>
+#ifdef __APPLE__
+#include <SDL2_ttf/SDL_ttf.h>
 #include <SDL2_image/SDL_image.h>
+#include <SDL2_mixer/SDL_mixer.h>
+#include <SDL2_net/SDL_net.h>
+#else
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_mixer.h>
+#include <SDL2/SDL_net.h>
+#endif
 void LobbyWindow();
 #endif

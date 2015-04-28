9f7af57e1a00542152e4866c4439acbbd52099cb
SL4GG4N <eddie.lowenborg@live.se>
2015-04-28 13:53:52 +0200

    it works

--- BJORNSTHEGAME/Client/lobby/lobby.c
+++ BJORNSTHEGAME/Client/lobby/lobby.c
@@ -10,78 +10,72 @@ void LobbyWindow(){
 	SDL_Surface* lobbyBackground = IMG_Load("lobby.png");
     // image for ready button
 	SDL_Surface* readyButton = IMG_Load("ready.png");
+    SDL_Rect buttonPlacement;
     
-    SDL_Rect buttonPlacement = {((lobbySurface->w/3)-240),
-                                ((lobbySurface->h/2)+200),
-                                400,
-                                80
-                                };
+    
 
     // mousePosition(X-axis,Y-axis)
     int mousePosition[2] = {0, 0};
     int endLobby = 0;
 
-    while(!endLobby){
-        
-	    //Initialize SDL
-        if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-    	{
-        	printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
-    	}
-
-        else
+    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
         {
-        //Create window
-            lobby = SDL_CreateWindow(   "BJORNSLOBBY",
-                                        SDL_WINDOWPOS_UNDEFINED,
-                                        SDL_WINDOWPOS_UNDEFINED,
-        		  				        1280,
-        	   	   				        800,
-                                        SDL_WINDOW_FULLSCREEN);
-            if( lobby == NULL )
-            	{
-                	printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
-            	}
-    	    else
-        	    {
-                	//Get window surface
-                	lobbySurface = SDL_GetWindowSurface( lobby );
-
-				    SDL_BlitScaled(lobbyBackground, NULL, lobbySurface, NULL);
-
-                    
-                    //place ready button on screen
-				    SDL_BlitScaled(readyButton, NULL, lobbySurface, &buttonPlacement);
-            	
-                    //Update the surface
-                	SDL_UpdateWindowSurface(lobby);
-            }
+            printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
         }
 
+    else
+    {
+        //Create window 
+    lobby = SDL_CreateWindow(   "BJORNSLOBBY",
+                            SDL_WINDOWPOS_UNDEFINED,
+                            SDL_WINDOWPOS_UNDEFINED,
+                            1280,
+                            800,
+                            SDL_WINDOW_FULLSCREEN);
+        if( lobby == NULL )
+            {
+                printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+            }
+        else
+            {
+                    //Get window surface
+            lobbySurface = SDL_GetWindowSurface( lobby );
+ 
+            //rectangle for button
+            buttonPlacement.x = (lobbySurface->w/3)-240;
+            buttonPlacement.y = (lobbySurface->h/2)+200;
+            buttonPlacement.w =  400;
+            buttonPlacement.h =  80;    
+            }
+    }
+
+    while(!endLobby){
         // Mouse events handling
         SDL_PumpEvents();
         SDL_GetMouseState(&mousePosition[0], &mousePosition[1]);
 
-        if(getMouseBounds(mousePosition, buttonPlacement))
+        if((  SDL_GetMouseState(NULL, NULL) & SDL_BUTTON(SDL_BUTTON_LEFT)  ) )
         {
-            if(  (  SDL_GetMouseState(NULL, NULL) & SDL_BUTTON(SDL_BUTTON_LEFT)  )  )
+            if(  getMouseBounds(mousePosition, buttonPlacement) )
             {
                 printf("PLAYER IS READY!\n");
                 endLobby = 1;
             }
         }
+        SDL_BlitScaled(lobbyBackground, NULL, lobbySurface, NULL);
+        SDL_BlitScaled(readyButton, NULL, lobbySurface, &buttonPlacement);
+         //Update the surface
+        SDL_UpdateWindowSurface(lobby);
 
     }  
     SDL_Delay(5000);
 
-    printf("Shut down i progress\n");
+    printf("Shut down in progress\n");
     SDL_DestroyWindow(lobby); //Destroy window
     
     SDL_FreeSurface(lobbySurface);
     SDL_FreeSurface(lobbyBackground);
     SDL_FreeSurface(readyButton);
-
-    SDL_Delay(3000);
         
     SDL_Quit(); //Quit SDL subsystems
 }

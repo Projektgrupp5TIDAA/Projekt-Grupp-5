aa801c3cfffe2a0dba6b0dfec4a67985b122b88e
jonathankahre <jonte.k_1993@hotmail.com>
2015-04-29 09:59:26 +0200

    Currently sorting the folders, stand by

--- BJORNSTHEGAME/Shared/bjornshared.c
+++
@@ -1,9 +0,0 @@
-#include "bjornshared.h"
-
-int is_in_use(int comp, int bit){
-	return comp & 1<<bit;
-}
-
-int is_in_use(int comp, int bit){
-	return comp & 1<<bit;
-}
--- BJORNSTHEGAME/Shared/bjornshared.h
+++
@@ -1,9 +0,0 @@
-#ifndef BJORN_SHARED_H
-#define BJORN_SHARED_H
-#include <stdio.h>
-
-int is_in_use(int comp, int bit);
-
-void set_bit(int *num, int bit);
-
-#endif

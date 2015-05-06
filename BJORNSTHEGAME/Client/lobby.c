4c659fec05e4e86afd3be8c0434bb55ed7831b58
SL4GG4N <eddie.lowenborg@live.se>
2015-05-06 15:35:54 +0200

    ändrat dekl.
    ska gå mer smooth

--- BJORNSTHEGAME/Client/lobby.c
+++ BJORNSTHEGAME/Client/lobby.c
@@ -1,4 +1,25 @@
+
+#ifndef _LOBBY_
+#define _LOBBY_
+#include <stdio.h>
+#include <SDL2/SDL.h>
+#include <string.h>
+#ifdef __APPLE__
+#include <SDL2_ttf/SDL_ttf.h>
+#include <SDL2_image/SDL_image.h>
+#include <SDL2_mixer/SDL_mixer.h>
+#include <SDL2_net/SDL_net.h>
+#else
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_mixer.h>
+#include <SDL2/SDL_net.h>
+#endif
+#include "bjornstartup.h"
+#include <time.h>
 #include "lobby.h"
+#endif
+
 
 int LobbyWindow(StartInfo lobbyConnection){
     //********************** INIT *************************
--- BJORNSTHEGAME/Client/lobby.h
+++ BJORNSTHEGAME/Client/lobby.h
@@ -1,24 +1,3 @@
-#ifndef _LOBBY_
-#define _LOBBY_
-#include <stdio.h>
-#include <SDL2/SDL.h>
-#include <string.h>
-#ifdef __APPLE__
-#include <SDL2_ttf/SDL_ttf.h>
-#include <SDL2_image/SDL_image.h>
-#include <SDL2_mixer/SDL_mixer.h>
-#include <SDL2_net/SDL_net.h>
-#else
-#include <SDL2/SDL_ttf.h>
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_mixer.h>
-#include <SDL2/SDL_net.h>
-#endif
-#include "bjornstartup.h"
-#include <time.h>
-
-int LobbyWindow(StartInfo lobbyConnection);
-
 typedef struct clockInformation
 {
     int seconds_left;
@@ -31,8 +10,7 @@ typedef struct{
     char names[6][20];
 }nrecv;
 
+int LobbyWindow(StartInfo lobbyConnection);
 void TimeThread(SDL_Window* screen);
 
 int Players_names(StartInfo players , SDL_Window* lobbyscreen, SDL_Surface* lobbySurface); 
-
-#endif

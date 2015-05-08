80d48e76af680a936aee021c69d607a3813d4ece
nooraa <noora-70@live.se>
2015-05-08 14:16:55 +0200

    ..

--- BJORNSTHEGAME/Client/gameplay/gameplay.h
+++ BJORNSTHEGAME/Client/gameplay/gameplay.h
@@ -2,6 +2,7 @@
 #define GAMEPLAY_H_INCLUDED
 #include <stdio.h>
 #include <SDL2/SDL.h>
+#include <stdbool.h>
 #ifdef __APPLE__
 #include <SDL2_ttf/SDL_ttf.h>
 #include <SDL2_image/SDL_image.h>
@@ -12,6 +13,11 @@
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_mixer.h>
 #include <SDL2/SDL_net.h>
+#define SPEEDx 10
 #endif
+
 void gameplayWindow();
+
+
 #endif // GAMEPLAY_H_INCLUDED
+

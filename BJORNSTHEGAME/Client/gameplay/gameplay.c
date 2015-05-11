1839ad83d529892ecc82764be0118b38b41813d5
jonathankahre <jonte.k_1993@hotmail.com>
2015-05-11 21:06:10 +0200

    Some position fixes; Gameplay

--- BJORNSTHEGAME/Client/gameplay/gameplay.c
+++ BJORNSTHEGAME/Client/gameplay/gameplay.c
@@ -4,16 +4,17 @@ void gameplayWindow()
 {
     int i;
     int size1=14, size2=3, size3=2;
+
     /*Loading and declaration of all images*/
     SDL_Window* gameplay;
-    SDL_Surface* gameBackground = IMG_Load("bar.jpg");;
-    SDL_Surface* ground =IMG_Load("ground2.png");
-    SDL_Surface*platform1 = IMG_Load("platform_hor.png");
-    SDL_Surface* platform2 = IMG_Load("platform_ver.png");
-    SDL_Surface* band = IMG_Load("band2.png");
-    SDL_Surface* playerSurface = IMG_Load("spriteBlue.PNG");
-    SDL_Surface*  bjorns=IMG_Load("bjorndrapare.png");
-    SDL_Surface*  ammo=IMG_Load("caps.png");
+    SDL_Surface* gameBackground = IMG_Load("../../Images/game/bar.jpg");;
+    SDL_Surface* ground = IMG_Load("../../Images/game/ground2.png");
+    SDL_Surface* platform1 = IMG_Load("../../Images/game/platform_hor.png");
+    SDL_Surface* platform2 = IMG_Load("../../Images/game/platform_ver.png");
+    SDL_Surface* band = IMG_Load("../../Images/game/band2.png");
+    SDL_Surface* playerSurface = IMG_Load("../../Images/game/spriteBlue.PNG");
+    SDL_Surface* bjorns = IMG_Load("../../Images/game/bjorndrapare.png");
+    SDL_Surface* ammo = IMG_Load("../../Images/game/caps.png");
     SDL_Surface* textSurface[size2];
 
     /*Texture declaration*/
@@ -41,7 +42,7 @@ void gameplayWindow()
 
 
 
-    /*Fill the platforms with colors */
+    /* Fill the platforms with colors */
     SDL_FillRect(platform1, NULL, SDL_MapRGB(platform1->format, 200, 190, 200));
     SDL_FillRect(platform2, NULL, SDL_MapRGB(platform2->format, 200, 190, 200));
 
@@ -58,7 +59,7 @@ void gameplayWindow()
         gameplay = SDL_CreateWindow("BJORNSGAMEPLAY", SDL_WINDOWPOS_UNDEFINED,
                                     SDL_WINDOWPOS_UNDEFINED,
                                     1280,800,
-                                    SDL_WINDOW_FULLSCREEN);
+                                    0);//SDL_WINDOW_FULLSCREEN_DESKTOP);
         if(gameplay == NULL)
         {
             printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
@@ -70,13 +71,13 @@ void gameplayWindow()
 
 
 
-            gRenderer =SDL_CreateRenderer(gameplay, -1, SDL_RENDERER_ACCELERATED); //Create a Render for the window
-            bakgroundTexture= SDL_CreateTextureFromSurface(gRenderer,gameBackground); //Load a texture background to the render
+            gRenderer = SDL_CreateRenderer(gameplay, -1, SDL_RENDERER_ACCELERATED); //Create a Render for the window
+            bakgroundTexture = SDL_CreateTextureFromSurface(gRenderer,gameBackground); //Load a texture background to the render
 
 
             /*text*/
             SDL_Color colorT= {170,60,255};
-            TTF_Font *font = TTF_OpenFont("StencilStd.ttf", 35);
+            TTF_Font *font = TTF_OpenFont("../../Images/game/StencilStd.ttf", 100);
             if(font == NULL)
             {
                 printf("FONT E NULL\n");
@@ -84,7 +85,7 @@ void gameplayWindow()
 
             textSurface[0]= TTF_RenderText_Solid(font, "Ammo:", colorT);
             textSurface[1]= TTF_RenderText_Solid(font, "HP:", colorT);
-            textSurface[2]= TTF_RenderText_Solid(font, "Drunk level:", colorT);
+            textSurface[2]= TTF_RenderText_Solid(font, "Drunk:", colorT);
 
             player = SDL_CreateTextureFromSurface(gRenderer, playerSurface); //the texture of the player
             /*creating texture for all the images and texts */
@@ -120,15 +121,15 @@ void gameplayWindow()
 
             /*make a rectangle for every texture to loading images in specific position on the screen*/
 
-            platforms[0].x = screen->w/5 -300;
-            platforms[0].y =screen ->h -65;
-            platforms[0].w = 1920;
-            platforms[0].h =70;
+            platforms[0].x = 0;
+            platforms[0].y = screen->h-(screen->h*0.06);
+            platforms[0].w = screen->w;
+            platforms[0].h = screen->h*0.06;
 
-            platforms[1].x = screen->w/5 -300;
-            platforms[1].y =screen ->h/2 -390;
-            platforms[1].w = 1920;
-            platforms[1].h = 90;
+            platforms[1].x = 0;
+            platforms[1].y = 0;
+            platforms[1].w = screen->w;
+            platforms[1].h = screen->h*0.08;
 
             platforms[2].x = screen->w/3 -200;
             platforms[2].y = screen->h - 180;
@@ -191,7 +192,13 @@ void gameplayWindow()
             platforms[13].h = 30;
 
 
-            capsRect[0].x = screen ->w/2 +600;
+            for(i=0;i<3;i++){
+                capsRect[i].x = screen->w*0.9+(i*screen->w*0.032);
+                capsRect[i].y = screen->h*0.02;//screen ->h - 740;
+                capsRect[i].w = screen->w*0.03;
+                capsRect[i].h = screen->h*0.046;
+            }
+            /*capsRect[0].x = screen ->w/2 +600;
             capsRect[0].y = screen ->h - 740;
             capsRect[0].w = 60;
             capsRect[0].h = 50;
@@ -204,32 +211,32 @@ void gameplayWindow()
             capsRect[2].x = screen ->w/2 +460;
             capsRect[2].y = screen ->h - 740;
             capsRect[2].w = 60;
-            capsRect[2].h = 50;
+            capsRect[2].h = 50;*/
 
-            bjornDRect[0].x = screen  ->w/2 -110;
-            bjornDRect[0].y = screen ->h - 228;
-            bjornDRect[0].w = 60;
-            bjornDRect[0].h = 50;
+            bjornDRect[0].x = screen->w/2 -110;
+            bjornDRect[0].y = screen->h - 228;
+            bjornDRect[0].w = screen->w*0.03;//60;
+            bjornDRect[0].h = screen->h*0.046;//50;
 
             bjornDRect[1].x = screen  ->w/2 +400;
             bjornDRect[1].y = screen ->h - 512;
-            bjornDRect[1].w = 60;
-            bjornDRect[1].h = 50;
+            bjornDRect[1].w = screen->w*0.03;//60;
+            bjornDRect[1].h = screen->h*0.046;//50;
 
-            textRect[0].x= screen ->w/2 +310;
-            textRect[0].y= screen ->h - 740;
-            textRect[0].w= 120;
-            textRect[0].h= 60;
+            textRect[0].x= screen->w*0.79;//screen ->w/2 +310;
+            textRect[0].y= screen->h*0.02;//screen ->h - 740;
+            textRect[0].w= screen->w*0.1;//120;
+            textRect[0].h= screen->h*0.055;//60;
 
-            textRect[1].x= screen ->w/2 -660;
-            textRect[1].y= screen ->h - 740;
-            textRect[1].w= 90;
-            textRect[1].h= 60;
+            textRect[1].x= screen->w*0.02;//screen ->w/2 -660;
+            textRect[1].y= screen->h*0.02;//screen ->h - 740;
+            textRect[1].w= screen->w*0.06;
+            textRect[1].h= screen->h*0.055;
 
-            textRect[2].x= screen ->w/2 -450;
-            textRect[2].y= screen ->h - 740;
-            textRect[2].w= 230;
-            textRect[2].h= 60;
+            textRect[2].x= screen->w*0.14;//screen->w/2 -450;
+            textRect[2].y= screen->h*0.02;
+            textRect[2].w= screen->w*0.11;
+            textRect[2].h= screen->h*0.055;
 
 
 
@@ -241,8 +248,8 @@ void gameplayWindow()
             SDL_Rect position;
             position.y = screen-> h/2 +110;
             position.x = screen->w/2 +120;
-            position.h = 100;
-            position.w = 110;
+            position.h = screen->h*0.11;//120;
+            position.w = screen->w*0.034;//66;
 
             //start frame and be facing to the right
             int frame = 2;

79ec9da5d7a6f7d12929d83b55aeb1a7910353c9
nooraa <noora-70@live.se>
2015-05-26 16:32:33 +0200

    boardscore update

--- BJORNSTHEGAME/Client/animation.c
+++ BJORNSTHEGAME/Client/animation.c
@@ -24,17 +24,20 @@
 #define PLAYERCOUNT 6
 #endif
 
-int animate(void* info){
+int animate(void* info)
+{
     animationInfo* animator = (animationInfo*) info;
     int i, flickerx=0, flickery=0;
     SDL_RendererFlip bflip = SDL_FLIP_NONE;
+    char *buffer[6]= {NULL};
+    char c[sizeof(int)];
 
     //Create the window
     animator->window = SDL_CreateWindow("BJORNS THE GAME",
-        SDL_WINDOWPOS_UNDEFINED,
-        SDL_WINDOWPOS_UNDEFINED,
-        1280,700,
-        0);//SDL_WINDOW_FULLSCREEN_DESKTOP);
+                                        SDL_WINDOWPOS_UNDEFINED,
+                                        SDL_WINDOWPOS_UNDEFINED,
+                                        1280,700,
+                                        0);//SDL_WINDOW_FULLSCREEN_DESKTOP);
     if(animator->window == NULL)
     {
         printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
@@ -56,6 +59,8 @@ int animate(void* info){
     SDL_Surface* textsurface[TEXTAMOUNT];
     SDL_Surface* rip= IMG_Load("../Images/game/rip.png");
     SDL_Surface* scoreboard= IMG_Load("../Images/game/deathboard.png");
+    SDL_Surface* deathsurface[PLAYERCOUNT];
+    SDL_Surface* scoreSurface[PLAYERCOUNT];
 
     SDL_Surface* playerSurface[6];
     playerSurface[5] = IMG_Load("../Images/game/spriteGreen.png");
@@ -78,6 +83,8 @@ int animate(void* info){
     SDL_Texture* bulletTex;
     SDL_Texture* rip_texture=NULL;
     SDL_Texture* score_texture= NULL;
+    SDL_Texture* deathText[PLAYERCOUNT];
+    SDL_Texture* death[PLAYERCOUNT];
 
     SDL_Rect spriteClips[4];
     SDL_Rect textSprite[6];
@@ -85,6 +92,7 @@ int animate(void* info){
     SDL_Rect capsRect[AMMOAMOUNT];
     SDL_Rect scores;
     SDL_Rect scorepos[PLAYERCOUNT];
+    SDL_Rect Namepos[PLAYERCOUNT];
 
     /* Fill the platforms with colors */
     SDL_FillRect(platform1, NULL, SDL_MapRGB(platform1->format, 200, 190, 200));
@@ -100,7 +108,8 @@ int animate(void* info){
     if(!Rend)
         printf("Couldn't start the render: %s\n", SDL_GetError());
 
-    for(i=0;i<6;i++){
+    for(i=0; i<6; i++)
+    {
         printf("Loading playertextures %d!\n", i);
         player[i] = SDL_CreateTextureFromSurface(Rend, playerSurface[i]);
     }
@@ -130,10 +139,10 @@ int animate(void* info){
     picture[1]= SDL_CreateTextureFromSurface(Rend,band);
     picture[2]= SDL_CreateTextureFromSurface(Rend,platform1);
     picture[3]= SDL_CreateTextureFromSurface(Rend,platform2);
-    
+
     score_texture= SDL_CreateTextureFromSurface(Rend, scoreboard);
 
-    for(i=4;i<PLATFORMAMOUNT;++i)
+    for(i=4; i<PLATFORMAMOUNT; ++i)
     {
         picture[i]= SDL_CreateTextureFromSurface(Rend,platform1);
     }
@@ -147,6 +156,46 @@ int animate(void* info){
     myText[1]=SDL_CreateTextureFromSurface(Rend,textsurface[1]);
     myText[2]=SDL_CreateTextureFromSurface(Rend,textsurface[2]);
     myText[3]=SDL_CreateTextureFromSurface(Rend,textsurface[3]);
+
+    SDL_Color black= {0,0,0};
+
+    for(i=0; i<PLAYERCOUNT; i++)
+    {
+        printf("loading deathsurface %d",i);
+        //     deathsurface[i]= TTF_RenderText_Blended(font,animator->test->names.names[5-i], black);
+        deathsurface[i]= TTF_RenderText_Blended(font,"PHOZ", black);
+    }
+    for(i=0; i<PLAYERCOUNT; i++)
+    {
+        deathText[i]=SDL_CreateTextureFromSurface(Rend,deathsurface[i]);
+    }
+
+
+    for(i=0; i<PLAYERCOUNT; i++)
+    {
+        //itoa(animator->players[i].deaths,buffer,10);
+        snprintf(c, sizeof(int), "%d", animator->players[i].deaths);
+        buffer[i] = malloc(sizeof(c));
+        strcpy(buffer[i], c);
+
+    }
+    for(i=0; i<PLAYERCOUNT; i++)
+    {
+
+        scoreSurface[i]=TTF_RenderText_Blended(font,buffer[i],black);
+
+
+    }
+
+    for(i=0; i<PLAYERCOUNT; i++)
+    {
+        if(death[i]==NULL)
+        {
+            printf("deth[i] fel");
+        }
+        death[i]=SDL_CreateTextureFromSurface(Rend,scoreSurface[i]);
+    }
+
 
     printf("All textures loaded.\n");
 
@@ -236,7 +285,8 @@ int animate(void* info){
     animator->platforms[16].w = 260;
     animator->platforms[16].h = 20;
 
-    for(i=0;i<AMMOAMOUNT;i++){
+    for(i=0; i<AMMOAMOUNT; i++)
+    {
         capsRect[i].x = screen->w*0.9+(i*screen->w*0.032);
         capsRect[i].y = screen->h*0.02;//screen ->h - 740;
         capsRect[i].w = screen->w*0.03;
@@ -282,44 +332,75 @@ int animate(void* info){
     textRect[4].y= screen->h*0.02;
     textRect[4].w= textsurface[3]->w/6;//screen->w*0.02;
     textRect[4].h= textsurface[3]->h;//screen->h*0.055;
-    
+
     scores.x= screen->w/3- 85;
     scores.y= screen->h/3;
     scores.h= screen->h/2;
     scores.w= screen->w/2;
-    
-    scorepos[0].x= screen->w/3*0.05;
-    scorepos[0].y= screen->h/3*0.1;
-    scorepos[0].h= screen->h/2*0.05;
-    scorepos[0].w= screen->w/2*0.05;
-    
-    scorepos[1].x= screen->w/2*0.05;
-    scorepos[1].y= screen->h/2*0.1;
-    scorepos[1].h= screen->h/3*0.05;
-    scorepos[1].w= screen->w/3*0.05;
-    
-    scorepos[2].x= screen->w/4*0.05;
-    scorepos[2].y= screen->h/4*0.1;
-    scorepos[2].h= screen->h*0.05;
-    scorepos[2].w= screen->w*0.05;
-    
-    scorepos[3].x= screen->w/3*0.01;
-    scorepos[3].y= screen->h/3*0.2;
-    scorepos[3].h= screen->h/2*0.05;
-    scorepos[3].w= screen->w/2*0.05;
-    
-    scorepos[4].x= screen->w*0.05;
-    scorepos[4].y= screen->h*0.3;
-    scorepos[4].h= screen->h/2*0.05;
-    scorepos[4].w= screen->w/2*0.05;
 
-    scorepos[5].x= screen->w/4*0.05;
-    scorepos[5].y= screen->h/4*0.1;
-    scorepos[5].h= screen->h/2*0.05;
-    scorepos[5].w= screen->w/2*0.05;
-    
+    Namepos[0].x= screen->w/2 -240;
+    Namepos[0].y= screen->h/2 +20;
+    Namepos[0].h= deathsurface[0]->h/2;
+    Namepos[0].w= deathsurface[0]->w;
+
+    Namepos[1].x= screen->w/2 -240;
+    Namepos[1].y= screen->h/2 +45;
+    Namepos[1].h= deathsurface[1]->h/2;
+    Namepos[1].w= deathsurface[1]->w;
+
+    Namepos[2].x= screen->w/2 -240;
+    Namepos[2].y= screen->h/2 +70;
+    Namepos[2].h= deathsurface[2]->h/2;
+    Namepos[2].w= deathsurface[2]->w;
+
+    Namepos[3].x= screen->w/2 -240;
+    Namepos[3].y= screen->h/2 +95;
+    Namepos[3].h= deathsurface[3]->h/2;
+    Namepos[3].w= deathsurface[3]->w;
+
+    Namepos[4].x= screen->w/2 -240;
+    Namepos[4].y= screen->h/2 +120;
+    Namepos[4].h= deathsurface[4]->h/2;
+    Namepos[4].w= deathsurface[4]->w;
+
+    Namepos[5].x= screen->w/2 -240;
+    Namepos[5].y= screen->h/2 +145;
+    Namepos[5].h= deathsurface[5]->h/2;
+    Namepos[5].w= deathsurface[5]->w;
+
+    scorepos[0].x= screen->w/2 +200;
+    scorepos[0].y= screen->h/2 +20;
+    scorepos[0].h= scoreSurface[0]->h/2;
+    scorepos[0].w= scoreSurface[0]->w;
+
+    scorepos[1].x= screen->w/2 +200;
+    scorepos[1].y= screen->h/2 +45;
+    scorepos[1].h= scoreSurface[1]->h/2;
+    scorepos[1].w= scoreSurface[1]->w;
+
+    scorepos[2].x= screen->w/2 +200;
+    scorepos[2].y= screen->h/2 +70;
+    scorepos[2].h= scoreSurface[2]->h/2;
+    scorepos[2].w= scoreSurface[2]->w;
+
+    scorepos[3].x= screen->w/2 +200;
+    scorepos[3].y= screen->h/2 +95;
+    scorepos[3].h= scoreSurface[3]->h/2;
+    scorepos[3].w= scoreSurface[3]->w;
+
+    scorepos[4].x= screen->w/2 +200;
+    scorepos[4].y= screen->h/2 +120;
+    scorepos[4].h= scoreSurface[4]->h/2;
+    scorepos[4].w= scoreSurface[4]->w;
+
+    scorepos[5].x= screen->w/2 +200;
+    scorepos[5].y= screen->h/2 +145;
+    scorepos[5].h= scoreSurface[5]->h/2;
+    scorepos[5].w= scoreSurface[5]->w;
+
     // Size and position for the player
-    for(i=0;i<PLAYERCOUNT;i++){
+    for(i=0; i<PLAYERCOUNT; i++)
+    {
         animator->players[i].pos.y = screen->h;
         animator->players[i].pos.x = screen->w/2+(i*50);
         animator->players[i].pos.h = screen->h*0.11;//120;
@@ -347,14 +428,16 @@ int animate(void* info){
     spriteClips[3].w = 210;
     spriteClips[3].h = 348;
 
-    for(i=0;i<6;i++){
+    for(i=0; i<6; i++)
+    {
         textSprite[i].x = i*(textsurface[3]->w/6);
         textSprite[i].y = 0;
         textSprite[i].w = (textsurface[3]->w/6);
         textSprite[i].h = textsurface[3]->h;
     }
 
-    for(i=0;i<PLAYERCOUNT;i++){
+    for(i=0; i<PLAYERCOUNT; i++)
+    {
         referenceplayer[i] = animator->players[i];
     }
 
@@ -370,11 +453,17 @@ int animate(void* info){
     SDL_FreeSurface(bjornsTom);
     SDL_FreeSurface(rip);
     SDL_FreeSurface(scoreboard);
-    for(i=0;i<6;i++){
+    for(i=0; i<6; i++)
+    {
         SDL_FreeSurface(playerSurface[i]);
     }
-    for(i=0;i<TEXTAMOUNT;i++){
+    for(i=0; i<TEXTAMOUNT; i++)
+    {
         SDL_FreeSurface(textsurface[i]);
+    }
+    for(i=0; i<PLAYERCOUNT; i++)
+    {   SDL_FreeSurface(deathsurface[i]);
+        SDL_FreeSurface(scoreSurface[i]);
     }
 
     printf("Surfaces have been unallocated.\n");
@@ -392,11 +481,15 @@ int animate(void* info){
         }
 
         //Render all of the active bullets on the screen to the buffer of the renderer
-        for(i=0; i<12 ;i++){
-            if(animator->bullets[i].TTL != 0){
-                if(animator->bullets[i].direction<0){
+        for(i=0; i<12 ; i++)
+        {
+            if(animator->bullets[i].TTL != 0)
+            {
+                if(animator->bullets[i].direction<0)
+                {
                     bflip = SDL_FLIP_NONE;
-                }else
+                }
+                else
                     bflip = SDL_FLIP_HORIZONTAL;
                 SDL_RenderCopyEx(Rend, bulletTex, NULL, &animator->bullets[i].pos, 0, NULL, bflip);
             }
@@ -428,45 +521,62 @@ int animate(void* info){
         SDL_RenderCopy(Rend, myText[3], &textSprite[*(animator->drunk)], &textRect[4]);
 
         //Render the players onto the buffer of the renderer aswell as manage the movement of the reference
-        for(i=0;i<PLAYERCOUNT;i++){
-            if(animator->players[i].health < 1){
+        for(i=0; i<PLAYERCOUNT; i++)
+        {
+            if(animator->players[i].health < 1)
+            {
                 SDL_RenderCopy(Rend, rip_texture, NULL, &animator->players[i].pos);
-            }else{
-                if(animator->players[i].dir == 1){
+            }
+            else
+            {
+                if(animator->players[i].dir == 1)
+                {
                     animator->flip = SDL_FLIP_NONE;
-                }else
+                }
+                else
                     animator->flip = SDL_FLIP_HORIZONTAL;
-                if(animator->players[i].pos.x != referenceplayer[i].pos.x){
-                    if(animator->players[i].pos.x > referenceplayer[i].pos.x){
+                if(animator->players[i].pos.x != referenceplayer[i].pos.x)
+                {
+                    if(animator->players[i].pos.x > referenceplayer[i].pos.x)
+                    {
                         referenceplayer[i].pos.x+=2;
                         flickerx = 1;
-                    }else{
+                    }
+                    else
+                    {
                         referenceplayer[i].pos.x-=2;
                         if(flickerx == 1)
                             flickerx=2;
                         else
                             flickerx=0;
                     }
-                    if(flickerx == 2){
+                    if(flickerx == 2)
+                    {
                         referenceplayer[i].pos.x = animator->players[i].pos.x;
                     }
                 }
-                if(animator->players[i].pos.y != referenceplayer[i].pos.y){
-                    if(animator->players[i].pos.y > referenceplayer[i].pos.y){
+                if(animator->players[i].pos.y != referenceplayer[i].pos.y)
+                {
+                    if(animator->players[i].pos.y > referenceplayer[i].pos.y)
+                    {
                         referenceplayer[i].pos.y+=2;
                         flickery = 1;
-                    }else{
+                    }
+                    else
+                    {
                         referenceplayer[i].pos.y-=2;
                         if(flickery == 1)
                             flickery=2;
                         else
                             flickery=0;
                     }
-                    if(flickery == 2){
+                    if(flickery == 2)
+                    {
                         referenceplayer[i].pos.y = animator->players[i].pos.y;
                     }
                 }
-                if(animator->players[i].pos.w != referenceplayer[i].pos.w){
+                if(animator->players[i].pos.w != referenceplayer[i].pos.w)
+                {
                     referenceplayer[i] = animator->players[i];
                 }
                 SDL_RenderCopyEx(Rend, player[i], &spriteClips[animator->frame], &referenceplayer[i].pos, 0, NULL, animator->flip);
@@ -475,21 +585,30 @@ int animate(void* info){
 
         //Render the present buffer of the renderer onto the screen
         SDL_RenderPresent(Rend);
-        
+
         SDL_Delay(1000/240);
     }
-    
-    if(*(animator->gameclock) == 0){
-        int l;
+
+    if(*(animator->gameclock) == 0)
+    {
+
         SDL_RenderCopy(Rend, score_texture, NULL, &scores);
-        /*for(l=0;l<PLAYERCOUNT;l++){
-            if(players[i] !=NULL)
-                SDL_RenderCopy(Rend, &players[i]->deaths, score_texture, NULL, scorepos[i]);
-        }*/
+
+         for(i=0; i<PLAYERCOUNT; i++)
+        {
+            SDL_RenderCopy(Rend, deathText[i], NULL, &Namepos[i]);
+        }
+
+        for(i=0; i<PLAYERCOUNT; i++)
+        {
+
+            SDL_RenderCopy(Rend,death[i],NULL,&scorepos[i]);
+
+        }
         SDL_RenderPresent(Rend);
         SDL_Delay(10000);
     }
-    
+
     /* Destroy all textures, the window and the renderer */
     for(i=0; i<PLATFORMAMOUNT; i++)
     {
@@ -507,8 +626,21 @@ int animate(void* info){
     SDL_DestroyTexture(bjornDTom);
     SDL_DestroyTexture(rip_texture);
     SDL_DestroyTexture(score_texture);
-    for(i=0;i<6;i++){
+    for(i=0; i<6; i++)
+    {
         SDL_DestroyTexture(player[i]);
+    }
+
+    for(i=0; i<PLAYERCOUNT; i++)
+    {
+        SDL_DestroyTexture(deathText[i]);
+
+    }
+
+    for(i=0; i<PLAYERCOUNT; i++)
+    {
+
+        SDL_DestroyTexture(death[i]);
     }
     SDL_DestroyRenderer(Rend);
     SDL_DestroyWindow(animator->window);
